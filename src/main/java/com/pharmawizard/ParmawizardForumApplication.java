870443267151f3c81e2ce0e305de21c6c4ca2ca6
Luca Speziale <lspeziale@gmail.com>
2016-11-30 14:23:03 +0100

    Primo invio

---
+++ src/main/java/com/pharmawizard/ParmawizardForumApplication.java
@@ -0,0 +1,12 @@
+package com.pharmawizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParmawizardForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParmawizardForumApplication.class, args);
+	}
+}
