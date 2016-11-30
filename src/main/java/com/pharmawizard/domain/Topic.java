cadd5e08fc8f835c020ceb0fe158439f0bebec86
lspeziale <lspeziale@gmail.com>
2016-11-30 16:49:44 +0100

    Topic

--- src/main/java/com/pharmawizard/domain/Topic.java
+++ src/main/java/com/pharmawizard/domain/Topic.java
@@ -2,17 +2,42 @@ package com.pharmawizard.domain;
 
 import java.sql.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="Topic")
 public class Topic {
 
-	private Long id;
+	@Id
+	@GeneratedValue
+	@Column(name="ID_TOPIC", nullable=false, unique=true)
+	private Long idTopic;
+	
+	@Column(name="TITLE", nullable=false, unique=true)
+	private String title;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="ID_USER")
+	private UserProfile author;
+	
+	@Column(name="DATE_CREATED", nullable=false, unique=false)
+	private Date date;
+	
+	@Column(name="VIEWS", nullable=false, unique=false)
+	private Long views;
+	
 	public Long getId() {
-		return id;
+		return idTopic;
 	}
 	public void setId(Long id) {
-		this.id = id;
+		this.idTopic = id;
 	}
 	public String getTitle() {
 		return title;
@@ -20,10 +45,10 @@ public class Topic {
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public User getAuthor() {
+	public UserProfile getAuthor() {
 		return author;
 	}
-	public void setAuthor(User author) {
+	public void setAuthor(UserProfile author) {
 		this.author = author;
 	}
 	public Date getDate() {
@@ -38,9 +63,5 @@ public class Topic {
 	public void setViews(Long views) {
 		this.views = views;
 	}
-	private String title;
-	private User author;
-	private Date date;
-	private Long views;
 	
 }
