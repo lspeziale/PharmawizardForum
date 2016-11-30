0583377fef8c593ca09d016396adfd272c6e0d93
lspeziale <lspeziale@gmail.com>
2016-11-30 17:12:13 +0100

    Merged

--- src/main/java/com/pharmawizard/domain/Topic.java
+++ src/main/java/com/pharmawizard/domain/Topic.java
@@ -12,54 +12,65 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="Topic")
+@Table(name = "Topic")
 public class Topic {
 
 	@Id
 	@GeneratedValue
-	@Column(name="ID_TOPIC", nullable=false, unique=true)
+	@Column(name = "ID_TOPIC", nullable = false, unique = true)
 	private Long idTopic;
-	
-	@Column(name="TITLE", nullable=false, unique=true)
+
+	@Column(name = "TITLE", nullable = false, unique = true)
 	private String title;
+
 	
 	@ManyToOne(fetch=FetchType.EAGER)
 	@JoinColumn(name="USER_PROFILE")
+
 	private UserProfile author;
-	
-	@Column(name="DATE_CREATED", nullable=false, unique=false)
+
+	@Column(name = "DATE_CREATED", nullable = false, unique = false)
 	private Date date;
-	
-	@Column(name="VIEWS", nullable=false, unique=false)
+
+	@Column(name = "VIEWS", nullable = false, unique = false)
 	private Long views;
-	
+
 	public Long getId() {
 		return idTopic;
 	}
+
 	public void setId(Long id) {
 		this.idTopic = id;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public UserProfile getAuthor() {
 		return author;
 	}
+
 	public void setAuthor(UserProfile author) {
 		this.author = author;
 	}
+
 	public Date getDate() {
 		return date;
 	}
+
 	public void setDate(Date date) {
 		this.date = date;
 	}
+
 	public Long getViews() {
 		return views;
 	}
+
 	public void setViews(Long views) {
 		this.views = views;
 	}
@@ -70,4 +81,71 @@ public class Topic {
 		this.idTopic = idTopic;
 	}
 	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + ((idTopic == null) ? 0 : idTopic.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((views == null) ? 0 : views.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Topic other = (Topic) obj;
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
+			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (idTopic == null) {
+			if (other.idTopic != null)
+				return false;
+		} else if (!idTopic.equals(other.idTopic))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (views == null) {
+			if (other.views != null)
+				return false;
+		} else if (!views.equals(other.views))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Topic [idTopic=");
+		builder.append(idTopic);
+		builder.append(", title=");
+		builder.append(title);
+		builder.append(", author=");
+		builder.append(author);
+		builder.append(", date=");
+		builder.append(date);
+		builder.append(", views=");
+		builder.append(views);
+		builder.append("]");
+		return builder.toString();
+	}
+
 }
