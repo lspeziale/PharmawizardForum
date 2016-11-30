57be5fd82de26a8ae691f839f753afb81ac574c4
Petru Buzulan <Petru Buzulan@DESKTOP-PPP3BBP>
2016-11-30 16:20:24 +0100

    UserProfile

--- src/main/java/com/pharmawizard/domain/UserProfile.java
+++ src/main/java/com/pharmawizard/domain/UserProfile.java
@@ -1,5 +1,87 @@
 package com.pharmawizard.domain;
 
+import javax.management.relation.Role;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USER_PROFILE")
 public class UserProfile {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_USER", nullable = false, updatable = false)
+	private Long idUser;
+
+	@Column(name = "NOME", nullable = false, unique = false)
+	private String nome;
+
+	@Column(name = "USERNAME", nullable = false, unique = true)
+	private String username;
+
+	@Column(name = "EMAIL", nullable = false, unique = true)
+	private String email;
+
+	@Column(name = "PASSWORD", nullable = false)
+	private String password;
+
+	@Column(name = "role", nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Role role;
+
+	public Long getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(Long idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
 
 }
