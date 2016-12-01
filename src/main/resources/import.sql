471160467ce07297394001af2bd8a55f9a73c427
lspeziale <lspeziale@gmail.com>
2016-12-01 14:51:56 +0100

    f

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,4 +6,5 @@ INSERT INTO pharmawizard_forum.user_role(type) VALUES ('DBA');
 
 INSERT INTO pharmawizard_forum.user_profile(username, password, name, email) VALUES ('sam','$2a$10$4eqIF5s/ewJwHK1p8lqlFOEm2QIA0S8g6./Lok.pQxqcxaBZYChRm', 'Sam','samy@xyz.com');
 
-INSERT INTO pharmawizard_forum.user_profile:user_role(id_user,id_role)VALUES (1,2)
+INSERT INTO pharmawizard_forum.user_profile_user_role(id_user,id_role)VALUES (1,2)
+
